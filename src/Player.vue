4b19be93d10f1d069f3b3b21c8ea1fa35d683614
ぐらふぃーむ <53967830+graphemecluster@users.noreply.github.com>
2020-03-20 14:10:47 +0800

    Add files via upload

---
+++ src/Player.vue
@@ -0,0 +1,70 @@
+<script>
+import Tone from "tone";
+import { INSTS } from "../config/const.js";
+
+export default {
+  name: "Player",
+  props: {
+    isActive: Boolean,
+    chordVoicing: Array,
+    settings: Object
+  },
+  data() {
+    return {
+      insts: [],
+      instSynth: new Tone.PolySynth().toMaster()
+    };
+  },
+  watch: {
+    chordVoicing: function() {
+      if (!this.isActive) return false;
+      if (!this.settings.isActiveHover) return false;
+      this.playChord();
+    }
+  },
+  methods: {
+    playChord: function() {
+      if (!this.chordVoicing || !this.chordVoicing.length) return false;
+      this.insts[this.settings.inst].volume.value = Tone.gainToDb(this.settings.volume / 100);
+      this.insts[this.settings.inst].releaseAll();
+      this.insts[this.settings.inst].triggerAttackRelease(this.chordVoicing.map(midi => Tone.Frequency(midi, "midi")), 1.5);
+      /*
+      const part = new Tone.Part(
+        function setPlay(time, note) {
+          this.insts[this.settings.inst].releaseAll();
+          this.insts[this.settings.inst].triggerAttackRelease(note, 1.5, time);
+        }.bind(this),
+        [["0:0:0", this.chordVoicing.map(midi => Tone.Frequency(midi, "midi"))]]
+      ).start();
+      Tone.Transport.start();
+      */
+    },
+    keyDown: function(e) {
+      if (!this.isActive) return false;
+      if (!this.settings.isActiveKey) return false;
+      if (!e.ctrlKey && !e.metaKey) return false;
+      if (e.keyCode === 32) {
+        this.playChord();
+      }
+    },
+    click: function(e) {
+      if (!this.isActive) return false;
+      if (!this.settings.isActiveClick) return false;
+      this.playChord();
+    }
+  },
+  mounted() {
+    INSTS.forEach(
+      function(v) {
+        if (v.isSample) {
+          this.insts[v.key] = new Tone.Sampler(v.samples).toMaster();
+        } else {
+          this.insts[v.key] = new Tone.PolySynth().toMaster();
+        }
+      }.bind(this)
+    );
+    window.addEventListener("keydown", this.keyDown);
+    window.addEventListener("click", this.click);
+  }
+};
+</script>
---
+++ src/Score.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="chord-dictionary-score"></div>
+</template>
+
+<script>
+import Vex from "vexflow";
+
+export default {
+  name: "ChordScore",
+  props: {
+    chordObject: Object
+  },
+  watch: {
+    chordObject: function(val) {
+      if (val) this.dispScore(val);
+    }
+  },
+  methods: {
+    dispScore: chordObject => {
+      const VF = Vex.Flow;
+
+      const div = document.getElementById("score");
+      if (div === null) return false;
+      div.textContent = null;
+
+      const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+      renderer.resize(150, 120);
+      const context = renderer.getContext();
+      context.setFont("Arial", 10, "").setBackgroundFillStyle("#fff");
+      const stave = new VF.Stave(0, -5, 120);
+      stave.addClef("treble");
+      stave.setContext(context).draw();
+
+      const notes = new VF.StaveNote({ keys: chordObject.display, duration: "w" });
+
+      chordObject.original.forEach((note, index) => {
+        if (note.offset) notes.addAccidental(index, new VF.Accidental(note.offset < 0 ? "b".repeat(-note.offset) : "#".repeat(note.offset)));
+      });
+
+      const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+      voice.addTickables([notes]);
+      new VF.Formatter().joinVoices([voice]).format([voice], 800);
+      voice.draw(context, stave);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#chord-dictionary-score {
+  margin-top: 10px;
+  height: 100px;
+}
+</style>
