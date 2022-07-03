7331653a908d589a0e49188f1ca4bcf1e23bd9fc
ashcolor <ashcolor27@gmail.com>
2022-07-03 19:02:01 +0900

    chg: 不要なコメントの削除

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import VueI18n from "vue-i18n";
-
 import langs from "./config/i18n";
 
-// Install BootstrapVue
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
 Vue.use(VueI18n);
@@ -20,12 +17,12 @@ const wrapperEl = document.getElementById("chord-dictionary-wrapper");
 if (wrapperEl) wrapperEl.remove();
 document.body.appendChild(el);
 new Vue({
-  render: h => h(App),
-  i18n: new VueI18n({
-    locale: "en",
-    messages: langs
-  })
+    render: (h) => h(App),
+    i18n: new VueI18n({
+        locale: "en",
+        messages: langs,
+    }),
 }).$mount("#chord-dictionary-app");
-window.addEventListener("beforeunload", function() {
-  chrome.runtime.sendMessage(null, "reload");
+window.addEventListener("beforeunload", function () {
+    chrome.runtime.sendMessage(null, "reload");
 });
