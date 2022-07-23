e3834c61e97d57126c51ab43274eba7b17b7f089
ashcolor <ashcolor27@gmail.com>
2022-07-23 13:12:52 +0900

    add: <script setup>の追加

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,96 +1,86 @@
-<script>
+<script setup>
 import Tone from "tone";
 import { INSTS } from "../config/const.js";
+</script>
 
+<script>
 export default {
-  name: "Player",
-  props: {
-    isActive: Boolean,
-    showChord: Boolean,
-    chord: Object,
-    settings: Object
-  },
-  data() {
-    return {
-      insts: {}
-    };
-  },
-  watch: {
-    showChord: function(newVal, oldVal) {
-      if (!this.settings.isActiveHover) return false;
-      this.playChord();
+    props: {
+        isActive: Boolean,
+        showChord: Boolean,
+        chord: Object,
+        settings: Object,
     },
-    settings: {
-      handler: function(val) {
-        Tone.Master.volume.value = Tone.gainToDb(val.vol);
-      },
-      deep: true
-    }
-  },
-  methods: {
-    playChord: function() {
-      if (
-        !this.isActive ||
-        this.$parent.$el.matches(":hover") ||
-        !this.chord.voicing
-      )
-        return false;
-      Tone.Transport.stop().cancel();
-      try {
-        this.insts[this.settings.inst].releaseAll();
-      } catch (e) {}
-      this.chord.voicing.forEach(
-        function(midi, index) {
-          Tone.Transport.scheduleOnce(
-            function(time) {
-              this.insts[this.settings.inst].triggerAttack(
-                Tone.Frequency(midi, "midi"),
-                time
-              );
-            }.bind(this),
-            this.settings.isArpeggio * this.settings.arpeggio * index
-          );
-        }.bind(this)
-      );
-      Tone.Transport.scheduleOnce(
-        function(time) {
-          try {
-            this.insts[this.settings.inst].releaseAll(time);
-          } catch (e) {}
-        }.bind(this),
-        this.settings.duration
-      );
-      Tone.Transport.start();
+    data() {
+        return {
+            insts: {},
+        };
     },
-    keyDown: function(e) {
-      if (!this.settings.isActiveKey) return false;
-      if (window.navigator.platform.includes("Mac") ? !e.metaKey : !e.ctrlKey)
-        return false;
-      if (
-        !e.shiftKey ||
-        e.altKey ||
-        e.repeat ||
-        (e.which || e.keyCode || e.charCode) !== 32
-      )
-        return false;
-      this.playChord();
+    watch: {
+        showChord: function (newVal, oldVal) {
+            if (!this.settings.isActiveHover) return false;
+            this.playChord();
+        },
+        settings: {
+            handler: function (val) {
+                Tone.Master.volume.value = Tone.gainToDb(val.vol);
+            },
+            deep: true,
+        },
     },
-    click: function(e) {
-      if (!this.settings.isActiveClick) return false;
-      this.playChord();
-    }
-  },
-  mounted() {
-    INSTS.forEach(
-      function(v) {
-        this.insts[v.key] = (v.samples
-          ? new Tone.Sampler(v.samples)
-          : new Tone.PolySynth(12)
-        ).toMaster();
-      }.bind(this)
-    );
-    window.addEventListener("keydown", this.keyDown);
-    window.addEventListener("click", this.click);
-  }
+    methods: {
+        playChord: function () {
+            if (!this.isActive || this.$parent.$el.matches(":hover") || !this.chord.voicing)
+                return false;
+            Tone.Transport.stop().cancel();
+            try {
+                this.insts[this.settings.inst].releaseAll();
+            } catch (e) {}
+            this.chord.voicing.forEach(
+                function (midi, index) {
+                    Tone.Transport.scheduleOnce(
+                        function (time) {
+                            this.insts[this.settings.inst].triggerAttack(
+                                Tone.Frequency(midi, "midi"),
+                                time
+                            );
+                        }.bind(this),
+                        this.settings.isArpeggio * this.settings.arpeggio * index
+                    );
+                }.bind(this)
+            );
+            Tone.Transport.scheduleOnce(
+                function (time) {
+                    try {
+                        this.insts[this.settings.inst].releaseAll(time);
+                    } catch (e) {}
+                }.bind(this),
+                this.settings.duration
+            );
+            Tone.Transport.start();
+        },
+        keyDown: function (e) {
+            if (!this.settings.isActiveKey) return false;
+            if (window.navigator.platform.includes("Mac") ? !e.metaKey : !e.ctrlKey) return false;
+            if (!e.shiftKey || e.altKey || e.repeat || (e.which || e.keyCode || e.charCode) !== 32)
+                return false;
+            this.playChord();
+        },
+        click: function (e) {
+            if (!this.settings.isActiveClick) return false;
+            this.playChord();
+        },
+    },
+    mounted() {
+        INSTS.forEach(
+            function (v) {
+                this.insts[v.key] = (
+                    v.samples ? new Tone.Sampler(v.samples) : new Tone.PolySynth(12)
+                ).toMaster();
+            }.bind(this)
+        );
+        window.addEventListener("keydown", this.keyDown);
+        window.addEventListener("click", this.click);
+    },
 };
 </script>
--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,101 +1,95 @@
-<template>
-  <div id="chord-dictionary-score"></div>
-</template>
+<script setup>
+import { CLEFS, NOTES, COLORS } from "../config/const";
+import Vex from "vexflow";
+</script>
 
 <script>
-import { CLEFS, NOTES, COLORS } from "../config/const";
-
-import Vex from "vexflow";
-
 export default {
-  name: "ChordScore",
-  props: {
-    chord: Object,
-    settings: Object
-  },
-  watch: {
-    chord: function(newVal, oldVal) {
-      if (newVal.string === oldVal.string) return false;
-      this.dispScore();
-    }
-  },
-  mounted() {
-    this.dispScore();
-  },
-  methods: {
-    dispScore: function() {
-      if (!this.chord.original) return;
-      const div = document.getElementById("chord-dictionary-score");
-      if (!div) return;
-      div.textContent = "";
+    props: {
+        chord: Object,
+        settings: Object,
+    },
+    watch: {
+        chord: function (newVal, oldVal) {
+            if (newVal.string === oldVal.string) return false;
+            this.dispScore();
+        },
+    },
+    mounted() {
+        this.dispScore();
+    },
+    methods: {
+        dispScore: function () {
+            if (!this.chord.original) return;
+            const div = document.getElementById("chord-dictionary-score");
+            if (!div) return;
+            div.textContent = "";
 
-      const VF = Vex.Flow;
-      const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-      const context = renderer.getContext();
-      const stave = new VF.Stave(0, 0, 0);
-      stave.addClef(this.settings.clef);
-      stave.setContext(context);
+            const VF = Vex.Flow;
+            const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+            const context = renderer.getContext();
+            const stave = new VF.Stave(0, 0, 0);
+            stave.addClef(this.settings.clef);
+            stave.setContext(context);
 
-      var octave =
-        CLEFS[this.settings.clef].octave +
-        (this.chord.original[0].key < CLEFS[this.settings.clef].note);
-      const notes = new VF.StaveNote({
-        clef: this.settings.clef,
-        keys: this.chord.original.map(
-          function(item, index) {
-            return (
-              "CDEFGAB".charAt(item.key) +
-              "/" +
-              (this.chord.original[index - 1] &&
-              item.key <= this.chord.original[index - 1].key
-                ? ++octave
-                : octave)
+            var octave =
+                CLEFS[this.settings.clef].octave +
+                (this.chord.original[0].key < CLEFS[this.settings.clef].note);
+            const notes = new VF.StaveNote({
+                clef: this.settings.clef,
+                keys: this.chord.original.map(
+                    function (item, index) {
+                        return (
+                            "CDEFGAB".charAt(item.key) +
+                            "/" +
+                            (this.chord.original[index - 1] &&
+                            item.key <= this.chord.original[index - 1].key
+                                ? ++octave
+                                : octave)
+                        );
+                    }.bind(this)
+                ),
+                duration: NOTES[this.settings.note].duration,
+            });
+
+            this.chord.original.forEach((note, index) => {
+                if (note.offset) {
+                    const acci = note.offset < 0 ? "bb" : "##",
+                        repetition = Math.abs(note.offset) >> 1;
+                    for (var i = 0; i < repetition; i++)
+                        notes.addAccidental(index, new VF.Accidental(acci));
+                    if (note.offset & 1)
+                        notes.addAccidental(index, new VF.Accidental(note.offset < 0 ? "b" : "#"));
+                }
+                if (this.settings.isColorNote)
+                    notes.setKeyStyle(index, { fillStyle: COLORS[note.toHalf()] });
+            });
+
+            const voice = new VF.Voice({
+                num_beats: 1,
+                beat_value: NOTES[this.settings.note].num,
+            });
+            voice.addTickables([notes]);
+            new VF.Formatter().joinVoices([voice]).format([voice]);
+            voice.setStave(stave);
+
+            //音符の描画位置の調整
+            const box = voice.getBoundingBox();
+            renderer.resize(
+                box.w + 80,
+                Math.max(Math.abs(box.y) + box.h + 15 + (this.settings.note != "whole") * 15, 110)
             );
-          }.bind(this)
-        ),
-        duration: NOTES[this.settings.note].duration
-      });
+            stave.setWidth(box.w + 65);
+            stave.setY(Math.max(20 - box.y, 0));
 
-      this.chord.original.forEach((note, index) => {
-        if (note.offset) {
-          const acci = note.offset < 0 ? "bb" : "##",
-            repetition = Math.abs(note.offset) >> 1;
-          for (var i = 0; i < repetition; i++)
-            notes.addAccidental(index, new VF.Accidental(acci));
-          if (note.offset & 1)
-            notes.addAccidental(
-              index,
-              new VF.Accidental(note.offset < 0 ? "b" : "#")
-            );
-        }
-        if (this.settings.isColorNote)
-          notes.setKeyStyle(index, { fillStyle: COLORS[note.toHalf()] });
-      });
-
-      const voice = new VF.Voice({
-        num_beats: 1,
-        beat_value: NOTES[this.settings.note].num
-      });
-      voice.addTickables([notes]);
-      new VF.Formatter().joinVoices([voice]).format([voice]);
-      voice.setStave(stave);
-
-      //音符の描画位置の調整
-      const box = voice.getBoundingBox();
-      renderer.resize(
-        box.w + 80,
-        Math.max(
-          Math.abs(box.y) + box.h + 15 + (this.settings.note != "whole") * 15,
-          110
-        )
-      );
-      stave.setWidth(box.w + 65);
-      stave.setY(Math.max(20 - box.y, 0));
-
-      stave.draw();
-      voice.draw(context, stave);
-      this.$emit("updated");
-    }
-  }
+            stave.draw();
+            voice.draw(context, stave);
+            this.$emit("updated");
+        },
+    },
 };
 </script>
+
+<template>
+    <div id="chord-dictionary-score"></div>
+</template>
