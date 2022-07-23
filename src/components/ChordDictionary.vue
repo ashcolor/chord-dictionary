2f0fea5b4a3e5e623fdf4f54f6cf8d1ef0e8e7e9
ashcolor <ashcolor27@gmail.com>
2022-07-23 23:19:03 +0900

    chg: ChordDictionary.vueリファクタリング

--- src/components/ChordDictionary.vue
+++ src/components/ChordDictionary.vue
@@ -3,198 +3,186 @@ import ChordNote from "../assets/ChordNote.js";
 import Score from "./Score.vue";
 import Player from "./Player.vue";
 import Setting from "./Setting.vue";
-</script>
+import { onMounted, watch, ref, reactive, getCurrentInstance } from "vue";
 
-<script>
+const instance = getCurrentInstance();
+
+const isActive = ref(true);
+const chord = ref({});
+const settings = reactive({
+    isShow: null,
+    language: null,
+    key: 0,
+    offset: 0,
+    isTranspose: false,
+    transposeKey: 0,
+    transposeOffset: 0,
+    vol: 0.84,
+    delay: 600,
+    duration: 1.5,
+    arpeggio: 0.04,
+    clef: "treble",
+    note: "quarter",
+    inst: "piano",
+    isDelay: false,
+    isArpeggio: false,
+    isColorNote: false,
+    isColorNoteName: false,
+    isShowRoman: false,
+    isActiveClick: true,
+    isActiveKey: true,
+    isActiveHover: false,
+});
+const position = reactive({});
+const range = ref(null);
+const textNode = ref(null);
+const highlightPos = reactive({});
+const highlightRect = reactive({
+    isActive: false,
+    top: null,
+    left: null,
+    width: null,
+    height: null,
+    bottom: null,
+    right: null,
+});
+const pageX = ref(0);
+const pageY = ref(0);
+const clientX = ref(0);
+const clientY = ref(0);
+const timeoutId = ref(null);
+const showChord = ref(false);
+
 var offsetBase = document.createElement("div");
 offsetBase.style.position = "absolute";
 offsetBase.style.top = 0;
 offsetBase.style.left = 0;
 document.body.appendChild(offsetBase);
 
-export default {
-    data() {
-        return {
-            isActive: true,
-            chord: {},
-            settings: {
-                isShow: null,
-                language: null,
-                key: 0,
-                offset: 0,
-                isTranspose: false,
-                transposeKey: 0,
-                transposeOffset: 0,
-                vol: 0.84,
-                delay: 600,
-                duration: 1.5,
-                arpeggio: 0.04,
-                clef: "treble",
-                note: "quarter",
-                inst: "piano",
-                isDelay: false,
-                isArpeggio: false,
-                isColorNote: false,
-                isColorNoteName: false,
-                isShowRoman: false,
-                isActiveClick: true,
-                isActiveKey: true,
-                isActiveHover: false,
-            },
-            position: {},
-            range: null,
-            textNode: null,
-            highlightPos: {},
-            highlightRect: null,
-            pageX: 0,
-            pageY: 0,
-            clientX: 0,
-            clientY: 0,
-            timeoutId: null,
-            showChord: false,
-        };
-    },
-    watch: {
-        chord: function (val) {
-            if (!val.string) return;
-            this.range.setStart(this.textNode, val.position);
-            this.range.setEnd(this.textNode, val.position + val.string.length);
-            var rangeRect = this.range.getBoundingClientRect();
-            var offsetRect = offsetBase.getBoundingClientRect();
-            this.highlightRect = {
-                top: rangeRect.top - offsetRect.top,
-                left: rangeRect.left - offsetRect.left,
-                width: rangeRect.width,
-                height: rangeRect.height,
-            };
-            this.highlightRect.bottom = this.highlightRect.top + this.highlightRect.height;
-            this.highlightRect.right = this.highlightRect.left + this.highlightRect.width;
-            if (this.cursorInRect()) {
-                this.highlightPos = {
-                    top: this.highlightRect.top + "px",
-                    left: this.highlightRect.left + "px",
-                    width: this.highlightRect.width + "px",
-                    height: this.highlightRect.height + "px",
-                };
-                this.timeoutId = setTimeout(
-                    this.displayChord,
-                    this.settings.isDelay * this.settings.delay
-                );
-            } else {
-                this.chord = {};
-                this.highlightRect = null;
-            }
-        },
-    },
-    mounted() {
-        chrome.runtime.onMessage.addListener((object) => {
-            this.isActive = object.isActive;
-        });
-        chrome.storage.local.get(
-            "settings",
-            function (value) {
-                if (value) this.settings = Object.assign(this.settings, value.settings);
-                this.settings.language =
-                    this.settings.language ||
-                    (function (languages) {
-                        for (var i = 0; i < languages.length; i++) {
-                            if (!languages[i]) continue;
-                            if (/^zh-(hk|mo)/i.test(languages[i])) return "hk";
-                            if (/^zh-(tw|hant)/i.test(languages[i])) return "tw";
-                            if (/^zh/i.test(languages[i])) return "cn";
-                            if (/^ja/i.test(languages[i])) return "ja";
-                            if (/^ko/i.test(languages[i])) return "ko";
-                            if (/^en-(gb|ie|au|nz)/i.test(languages[i])) return "gb";
-                            if (/^en/i.test(languages[i])) return "en";
-                        }
-                        return "en";
-                    })(
-                        [].concat(
-                            window.navigator.language,
-                            window.navigator.userLanguage,
-                            window.navigator.browserLanguage,
-                            window.navigator.systemLanguage,
-                            window.navigator.languages
-                        )
-                    );
-                if (this.settings.isShow === null) this.settings.isShow = true;
-            }.bind(this)
-        );
-        window.addEventListener(
-            "mousemove",
-            function (e) {
-                this.pageX = Math.min(e.pageX, document.documentElement.scrollWidth);
-                this.pageY = Math.min(e.pageY, document.documentElement.scrollHeight);
-                this.clientX = e.clientX;
-                this.clientY = e.clientY;
-                if (this.highlightRect && this.cursorInRect()) this.updated();
-                else {
-                    if (this.timeoutId !== null) clearTimeout(this.timeoutId);
-                    this.showChord = false;
-                    this.chord = {};
-                    this.highlightRect = null;
-                    this.setPointedChord();
+watch(chord, (val) => {
+    if (!val.string) return;
+    range.value.setStart(textNode.value, val.position);
+    range.value.setEnd(textNode.value, val.position + val.string.length);
+    var rangeRect = range.value.getBoundingClientRect();
+    var offsetRect = offsetBase.getBoundingClientRect();
+    highlightRect.isActive = true;
+    highlightRect.top = rangeRect.top - offsetRect.top;
+    highlightRect.left = rangeRect.left - offsetRect.left;
+    highlightRect.width = rangeRect.width;
+    highlightRect.height = rangeRect.height;
+    highlightRect.bottom = highlightRect.top + highlightRect.height;
+    highlightRect.right = highlightRect.left + highlightRect.width;
+    if (cursorInRect()) {
+        highlightPos.top = highlightRect.top + "px";
+        highlightPos.left = highlightRect.left + "px";
+        highlightPos.width = highlightRect.width + "px";
+        highlightPos.height = highlightRect.height + "px";
+        timeoutId.value = setTimeout(displayChord, settings.isDelay * settings.delay);
+    } else {
+        chord.value = {};
+        highlightRect.isActive = false;
+    }
+});
+
+onMounted(() => {
+    chrome.runtime.onMessage.addListener((object) => {
+        isActive = object.isActive;
+    });
+    chrome.storage.local.get("settings", (value) => {
+        if (value) Object.assign(settings, value.settings);
+        settings.language =
+            settings.language ||
+            ((languages) => {
+                for (var i = 0; i < languages.length; i++) {
+                    if (!languages[i]) continue;
+                    if (/^zh-(hk|mo)/i.test(languages[i])) return "hk";
+                    if (/^zh-(tw|hant)/i.test(languages[i])) return "tw";
+                    if (/^zh/i.test(languages[i])) return "cn";
+                    if (/^ja/i.test(languages[i])) return "ja";
+                    if (/^ko/i.test(languages[i])) return "ko";
+                    if (/^en-(gb|ie|au|nz)/i.test(languages[i])) return "gb";
+                    if (/^en/i.test(languages[i])) return "en";
                 }
-            }.bind(this)
-        );
-    },
-    methods: {
-        setPointedChord: function () {
-            if (document.caretPositionFromPoint) {
-                this.range = document.caretPositionFromPoint(this.clientX, this.clientY);
-                if (!this.range) return;
-                this.textNode = this.range.offsetNode;
-            } else if (document.caretRangeFromPoint) {
-                this.range = document.caretRangeFromPoint(this.clientX, this.clientY);
-                if (!this.range) return;
-                this.textNode = this.range.startContainer;
-            } else return;
-            if (!this.textNode || this.textNode.nodeType !== 3 || this.$el.contains(this.textNode))
-                return;
-            if (!this.textNode.parentNode.matches(":hover")) return;
-            ChordNote.parseContent.transposeOn = this.settings.isTranspose;
-            ChordNote.parseContent.intervalNote = ChordNote.Note(
-                this.settings.key,
-                this.settings.offset
+                return "en";
+            })(
+                [].concat(
+                    window.navigator.language,
+                    window.navigator.userLanguage,
+                    window.navigator.browserLanguage,
+                    window.navigator.systemLanguage,
+                    window.navigator.languages
+                )
             );
-            ChordNote.parseContent.transposeTo = ChordNote.Note(
-                this.settings.transposeKey,
-                this.settings.transposeOffset
-            );
-            this.chord = ChordNote.parseContent(this.textNode.nodeValue, this.range.startOffset);
-        },
-        updated: function () {
-            if (!this.showChord || !this.chord.string) return;
-            var div = document.getElementById("chord-dictionary-pop-up");
-            if (!div) return;
-            var dimension = div.getBoundingClientRect();
-            this.position = {
-                top:
-                    (dimension.height + this.pageY - window.scrollY + 30 >
-                    document.documentElement.clientHeight
-                        ? this.pageY - dimension.height - 10
-                        : this.pageY + 20) + "px",
-                left:
-                    (dimension.width + this.pageX - window.scrollX + 30 >
-                    document.documentElement.clientWidth
-                        ? this.pageX - dimension.width - 10
-                        : this.pageX + 20) + "px",
-            };
-        },
-        cursorInRect: function () {
-            return (
-                this.pageY >= this.highlightRect.top &&
-                this.pageY <= this.highlightRect.bottom &&
-                this.pageX >= this.highlightRect.left &&
-                this.pageX <= this.highlightRect.right
-            );
-        },
-        displayChord: function () {
-            this.showChord = true;
-            this.updated();
-            this.timeoutId = null;
-        },
-    },
+        if (settings.isShow === null) settings.isShow = true;
+    });
+    window.addEventListener("mousemove", (e) => {
+        pageX.value = Math.min(e.pageX, document.documentElement.scrollWidth);
+        pageY.value = Math.min(e.pageY, document.documentElement.scrollHeight);
+        clientX.value = e.clientX;
+        clientY.value = e.clientY;
+        if (highlightRect.isActive && cursorInRect()) updated();
+        else {
+            if (timeoutId.value !== null) clearTimeout(timeoutId.value);
+            showChord.value = false;
+            chord.value = {};
+            highlightRect.isActive = false;
+            setPointedChord();
+        }
+    });
+});
+
+const setPointedChord = () => {
+    if (document.caretPositionFromPoint) {
+        range.value = document.caretPositionFromPoint(clientX.value, clientY.value);
+        if (!range.value) return;
+        textNode.value = range.value.offsetNode;
+    } else if (document.caretRangeFromPoint) {
+        range.value = document.caretRangeFromPoint(clientX.value, clientY.value);
+        if (!range.value) return;
+        textNode.value = range.value.startContainer;
+    } else return;
+    if (
+        !textNode.value ||
+        textNode.value.nodeType !== 3 ||
+        instance.proxy.$el.contains(textNode.value)
+    )
+        return;
+    if (!textNode.value.parentNode.matches(":hover")) return;
+    ChordNote.parseContent.transposeOn = settings.isTranspose;
+    ChordNote.parseContent.intervalNote = ChordNote.Note(settings.key, settings.offset);
+    ChordNote.parseContent.transposeTo = ChordNote.Note(
+        settings.transposeKey,
+        settings.transposeOffset
+    );
+    chord.value = ChordNote.parseContent(textNode.value.nodeValue, range.value.startOffset);
+};
+const updated = () => {
+    if (!showChord.value || !chord.value.string) return;
+    var div = document.getElementById("chord-dictionary-pop-up");
+    if (!div) return;
+    var dimension = div.getBoundingClientRect();
+    position.top =
+        (dimension.height + pageY.value - window.scrollY + 30 >
+        document.documentElement.clientHeight
+            ? pageY.value - dimension.height - 10
+            : pageY.value + 20) + "px";
+    position.left =
+        (dimension.width + pageX.value - window.scrollX + 30 > document.documentElement.clientWidth
+            ? pageX.value - dimension.width - 10
+            : pageX.value + 20) + "px";
+};
+const cursorInRect = () => {
+    return (
+        pageY.value >= highlightRect.top &&
+        pageY.value <= highlightRect.bottom &&
+        pageX.value >= highlightRect.left &&
+        pageX.value <= highlightRect.right
+    );
+};
+const displayChord = () => {
+    showChord.value = true;
+    updated();
+    timeoutId.value = null;
 };
 </script>
 
