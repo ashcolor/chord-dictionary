4d6d5e022a909b15e257b6225a6c2cde1bd8ef1f
ashcolor <ashcolor27@gmail.com>
2023-10-09 13:14:51 +0900

    chg: CSP設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json" assert { type: "json" };
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,10 @@ export default defineConfig({
             script: {
                 defineModel: true,
             },
+        }),
+        // @https://tomikyblog.com/vue-i18n-csp-header-unsafe_eval/
+        VueI18nPlugin({
+            jitCompilation: true,
         }),
         crx({ manifest }),
     ],
