bceaaea25f29e03e3b09b3558fe18afa5aa4290e
ashcolor <ashcolor27@gmail.com>
2024-09-13 19:34:56 +0900

    upd: Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,9 @@
 import { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-    framework: "@storybook/vue3-vite",
-    stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/**/*.mdx"],
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: ["@storybook/addon-docs", "@storybook/addon-controls"],
-
-    async viteFinal(config, options) {
-        return config;
-    },
+    framework: "@storybook/vue3-vite",
 };
 
 export default config;
--- src/components/ChordScore.stories.ts
+++ src/components/ChordScore.stories.ts
@@ -7,21 +7,14 @@ type Story = StoryObj<typeof ChordScore>;
 const chordName = "C";
 const chord = ChordNote.parseContent(chordName, 0);
 
-const meta: Meta<typeof ChordScore> = {
+const meta = {
     title: "Card/ChordScore",
     component: ChordScore,
-    render: (args) => ({
-        components: { ChordScore },
-        setup() {
-            return { args };
-        },
-        template: "<ChordScore v-bind='args'/>",
-    }),
     args: {
         chordName,
         chordOriginal: chord.original,
     },
-};
+} satisfies Meta<typeof ChordScore>;
 
 export default meta;
 
